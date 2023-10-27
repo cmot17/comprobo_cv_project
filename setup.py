5e639e2ddf00c19c62bcefab73768b5999cce431
Swasti Jain <swasti324@gmail.com>
2023-10-27 13:26:15 -0400

    fixed module error in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'comprobo_cv_project'
+package_name = "comprobo_cv_project"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='swasti',
-    maintainer_email='swasti324@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="swasti",
+    maintainer_email="swasti324@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'neato_fetch = comprobo_cv_project.neato_fetch:main'
-        ],
+        "console_scripts": ["neato_fetch = neato_fetch.neato_fetch:main"],
     },
 )
