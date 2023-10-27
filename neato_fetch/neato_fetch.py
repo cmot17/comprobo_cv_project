dbb81e6f7b64d35c6bdda22e61adf5a18c79c5bf
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-10-27 14:16:09 -0400

    add contour detection

--- neato_fetch/neato_fetch.py
+++ neato_fetch/neato_fetch.py
@@ -34,124 +34,84 @@ class NeatoFetch(Node):
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
 
     def loop_wrapper(self):
-        """This function takes care of calling the run_loop function repeatedly.
-        We are using a separate thread to run the loop_wrapper to work around
-        issues with single threaded executors in ROS2"""
         cv2.namedWindow("video_window")
         cv2.namedWindow("binary_window")
-        cv2.namedWindow("image_info")
-        cv2.namedWindow("binary_image")
 
-        self.red_lower_bound = 0
-        self.green_lower_bound = 0
-        self.blue_lower_bound = 0
-        self.red_upper_bound = 255
-        self.green_upper_bound = 255
-        self.blue_upper_bound = 255
+        self.hue_lower_bound = 27
+        self.saturation_lower_bound = 137
+        self.value_lower_bound = 97
+        self.hue_upper_bound = 40  # Hue ranges from 0-180 in OpenCV
+        self.saturation_upper_bound = 255
+        self.value_upper_bound = 255
 
-        self.hue_lower_bound = 0
+        cv2.createTrackbar("Hue Lower", "binary_window", self.hue_lower_bound, 180, self.set_hue_lower_bound)
+        cv2.createTrackbar("Saturation Lower", "binary_window", self.saturation_lower_bound, 255, self.set_saturation_lower_bound)
+        cv2.createTrackbar("Value Lower", "binary_window", self.value_lower_bound, 255, self.set_value_lower_bound)
+        cv2.createTrackbar("Hue Upper", "binary_window", self.hue_upper_bound, 180, self.set_hue_upper_bound)
+        cv2.createTrackbar("Saturation Upper", "binary_window", self.saturation_upper_bound, 255, self.set_saturation_upper_bound)
+        cv2.createTrackbar("Value Upper", "binary_window", self.value_upper_bound, 255, self.set_value_upper_bound)
 
-        # cv2.createTrackbar(
-        #     "red lower bound",
-        #     "binary_window",
-        #     self.red_lower_bound,
-        #     255,
-        #     self.set_red_lower_bound,
-        # )
-        # cv2.createTrackbar(
-        #     "green lower bound",
-        #     "binary_window",
-        #     self.green_lower_bound,
-        #     255,
-        #     self.set_green_lower_bound,
-        # )
-        # cv2.createTrackbar(
-        #     "blue lower bound",
-        #     "binary_window",
-        #     self.blue_lower_bound,
-        #     255,
-        #     self.set_blue_lower_bound,
-        # )
-        # cv2.createTrackbar(
-        #     "red upper bound",
-        #     "binary_window",
-        #     self.red_upper_bound,
-        #     255,
-        #     self.set_red_upper_bound,
-        # )
-        # cv2.createTrackbar(
-        #     "green upper bound",
-        #     "binary_window",
-        #     self.green_upper_bound,
-        #     255,
-        #     self.set_green_upper_bound,
-        # )
-        # cv2.createTrackbar(
-        #     "blue upper bound",
-        #     "binary_window",
-        #     self.blue_upper_bound,
-        #     255,
-        #     self.set_blue_upper_bound,
-        # )
         cv2.setMouseCallback("video_window", self.process_mouse_event)
         while True:
             self.run_loop()
             time.sleep(0.1)
 
-    # def set_red_lower_bound(self, val):
-    #     self.red_lower_bound = val
+    def set_hue_lower_bound(self, val):
+        self.hue_lower_bound = val
 
-    # def set_green_lower_bound(self, val):
-    #     self.green_lower_bound = val
+    def set_saturation_lower_bound(self, val):
+        self.saturation_lower_bound = val
 
-    # def set_blue_lower_bound(self, val):
-    #     self.blue_lower_bound = val
+    def set_value_lower_bound(self, val):
+        self.value_lower_bound = val
 
-    # def set_red_upper_bound(self, val):
-    #     self.red_upper_bound = val
+    def set_hue_upper_bound(self, val):
+        self.hue_upper_bound = val
 
-    # def set_green_upper_bound(self, val):
-    #     self.green_upper_bound = val
+    def set_saturation_upper_bound(self, val):
+        self.saturation_upper_bound = val
 
-    # def set_blue_upper_bound(self, val):
-    #     self.blue_lower_bound = val
-
-    # def set_hue_lower_bound(self, val):
-    #     self.hue_lower_bound = val
-
-    # def set_hue_upper_bound(self, val):
-    #     self.hue_upper_bound = val
+    def set_value_upper_bound(self, val):
+        self.value_upper_bound = val
 
     def process_mouse_event(self, event, x, y, flags, param):
-        """Process mouse events so that you can see the color values
-        associated with a particular pixel in the camera images"""
-        self.image_info_window = 255 * np.ones((500, 500, 3))
-        cv2.putText(
-            self.image_info_window,
-            "Color (b=%d,g=%d,r=%d)"
-            % (self.cv_image[y, x, 0], self.cv_image[y, x, 1], self.cv_image[y, x, 2]),
-            (5, 50),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 0, 0),
-        )
+        if event == cv2.EVENT_LBUTTONDOWN:
+            # Update this method to show the color info directly on the main window
+            b, g, r = self.cv_image[y, x]
+            hsv = cv2.cvtColor(self.cv_image[y:y+1, x:x+1], cv2.COLOR_BGR2HSV)[0][0]
+            info_text = "BGR: (%d, %d, %d), HSV: (%d, %d, %d)" % (b, g, r, hsv[0], hsv[1], hsv[2])
+            cv2.putText(self.cv_image, info_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     def run_loop(self):
-        # NOTE: only do cv2.imshow and cv2.waitKey in this function
-        if not self.cv_image is None:
+        if self.cv_image is not None:
+            self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
             self.binary_image = cv2.inRange(
                 self.hsv_image,
-                (self.blue_lower_bound, self.green_lower_bound, self.red_lower_bound),
-                (self.blue_upper_bound, self.green_upper_bound, self.red_upper_bound),
+                (self.hue_lower_bound, self.saturation_lower_bound, self.value_lower_bound),
+                (self.hue_upper_bound, self.saturation_upper_bound, self.value_upper_bound)
             )
             self.binary_image = cv2.erode(self.binary_image, None, iterations=2)
             self.binary_image = cv2.dilate(self.binary_image, None, iterations=2)
 
-            print(self.cv_image.shape)
+             # Find contours
+            contours, _ = cv2.findContours(self.binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                perimeter = cv2.arcLength(contour, True)
+                area = cv2.contourArea(contour)
+                
+                # Avoid division by zero and small contours
+                # cv2.drawContours(self.cv_image, [contour], 0, (0,255,0), 3)
+                print(f'Contour detected with perimeter {perimeter} and area {area}')
+                if perimeter > 0 and area > 50:
+                    circularity = 4 * np.pi * area / (perimeter * perimeter)
+                    print(f'Perimeter and area met threshold, circularity: {circularity}')
+                    if 0.25 <= circularity <= 1.2:  # Adjust thresholds as needed
+                        (x, y), radius = cv2.minEnclosingCircle(contour)
+                        center = (int(x), int(y))
+                        radius = int(radius)
+                        cv2.circle(self.cv_image, center, radius, (0, 255, 0), 2)
             cv2.imshow("video_window", self.cv_image)
             cv2.imshow("binary_window", self.binary_image)
-            if hasattr(self, "image_info_window"):
-                cv2.imshow("image_info", self.image_info_window)
             cv2.waitKey(5)
 
 
